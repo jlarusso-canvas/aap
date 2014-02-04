f8a1eaf8dc0b6b2323bd0522f3180cc776383127
Erin L <erinlee2007@u.northwestern.edu>
2014-02-04 15:59:06 -0500

    move from Aerial America Live Player files

---
+++ app/assets/javascripts/AerialAmerica.js
@@ -0,0 +1,38 @@
+if (typeof(AA) == 'undefined') AA = {}
+AA = (function(){
+  // Private
+
+  return {
+    render_small_state: function(paper_width, paper_height, el){
+      paper_width = (paper_width) ? paper_width : 140;
+      paper_height = (paper_height) ? paper_height : 140;
+      if (el) {
+        var state_container = $(el)
+      }
+      else {
+        var state_container = $(AA.$doc.find('.small-state-image')[0])
+      }
+      var state = state_container.attr('data-state');
+      var paper = new Raphael(state_container[0].id, paper_width, paper_height);
+
+      var path = paper.path(AA.map_svg[state]).attr({
+        "fill": "#87972f",
+        "stroke": "#484c2c",
+        "stroke-opacity": "1",
+        "stroke-linejoin": "round",
+        "stroke-miterlimit": "4",
+        "stroke-width": "1",
+        "stroke-dasharray": "none"
+      });
+      var path_texture = paper.path(AA.map_svg[state]).attr({
+        "fill": "url(/img/state_texture.png)",
+        "stroke-width": 0
+      });
+
+      var scale_string = AA.RaphaelHelpers.get_scale_to_fit_string(paper, path, 0, paper_width, paper_height);
+
+      AA.RaphaelHelpers.translate_to_center(paper, path, false, scale_string);
+      AA.RaphaelHelpers.translate_to_center(paper, path_texture, false, scale_string);
+    }
+  }
+})();
--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,6 +1,4 @@
 class PlayerController < ApplicationController
-  layout 'player'
-
   def index
   end
 end

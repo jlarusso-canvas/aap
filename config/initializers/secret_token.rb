61a88f4518f08c841e5c5c908045b7d30d9f78ee
Jesse La Russo <jlarusso@canvas.is>
2014-02-04 10:42:13 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AerialAmericaPlayer::Application.config.secret_key_base = '111605e4d1dbc7d39a3c9cb8586d1553fb0088dd1657e9eb5211abcf30930e9f57747e62eb519cf3ffa6444f19230d56ca3471fcd137363941fc3f0737a09008'
